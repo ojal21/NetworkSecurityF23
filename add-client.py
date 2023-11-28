06b2f0f04ad7cb0f85e16e9bf847442daa57eae0
ojal21 <ojal_guitar@yahoo.co.in>
2023-11-28 13:24:53 -0600

    key generation when new client added

--- add-client.py
+++ add-client.py
@@ -1,7 +1,9 @@
 from crypto_custom import hash
 from json_util import *
+import rsa
 
 file_path = "broker/passwords.json"
+key_path="client/keys"
 
 password_list = load_json_file(file_path)
 username = input("Enter username: ")
@@ -19,3 +21,11 @@ write_json_file(password_list, file_path)
 print("------ADDED BELOW ENTRY-------")
 print("USERNAME:", username)
 print("HASH: ", pwd_hash)
+
+public_key_c1, private_key_c1=rsa.newkeys(1024)
+
+with open(f"{key_path}/{username}_public.pen","wb") as f:
+    f.write(public_key_c1.save_pkcs1("PEM"))
+
+with open(f"{key_path}/{username}_private.pen","wb") as f:
+    f.write(private_key_c1.save_pkcs1("PEM"))
